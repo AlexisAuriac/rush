3516c2865221e4b95480155eecda7c61fcd590c2
alexis <alexis.auriac@epitech.eu>
2019-05-31 19:42:47 +0200

    Found a crate to use unistd, started using fork

--- src/shell.rs
+++ src/shell.rs
@@ -1,6 +1,8 @@
 use isatty;
+use std::collections::hash_map::HashMap;
 use std::env;
 use std::io::{stdout, Write};
+use std::process::Command;
 
 use crate::utility::split_no_empty;
 
@@ -11,6 +13,9 @@ pub struct Shell {
     pub tty: bool,
     pub stop: bool,
 }
+
+use nix::unistd::fork;
+use nix::unistd::ForkResult;
 
 impl Shell {
     pub fn new() -> Shell {
@@ -52,5 +57,47 @@ impl Shell {
                 return;
             }
         }
+
+        match fork() {
+            Ok(ForkResult::Parent { child }) => {
+                println!("parent of {}", child);
+                match nix::sys::wait::waitpid(Option(child, {})) {
+                    nix::sys::wait::WaitStatus::Exited(_, status) => println!("exited"),
+                }
+            }
+            Ok(ForkResult::Child) => {
+                println!("child with pid {}", nix::unistd::getpid());
+                std::process::exit(1);
+            }
+            Err(err) => println!("{:?}", err),
+        }
+
+        // Command::new("ls")
+        // .env_clear()
+        // .env("PATH", "/bin")
+        // .spawn()
+        // .expect("piss off");
+
+        // let filtered_env: HashMap<String, String> = env::vars()
+        //     .filter(|&(ref k, _)| k == "TERM" || k == "TZ" || k == "LANG" || k == "PATH")
+        //     .collect();
+
+        // let filtered_env: HashMap<String, String> = HashMap::new();
+
+        // Command::new(&command[0])
+        // .env("PATH", "/bin")
+        // .env_clear()
+        // .envs(&self.env)
+        // .envs(&filtered_env)
+        // .spawn()
+        // .expect(&format!("{}: Command not found.", command[0]));
+        // if let Err(err) = Command::new(&command[0])
+        //     .args(&["hello"])
+        //     .env("PATH", "/bin")
+        //     .spawn()
+        // {
+        //     println!("{:?}", err);
+        // }
+        println!("end command");
     }
 }
