4759a3a7f6e41bd8e3f99034f52a27532eed8035
alexis <alexis.auriac@epitech.eu>
2019-06-04 01:09:23 +0200

    The PATH is now used to exec commands

--- src/shell/handle_command.rs
+++ src/shell/handle_command.rs
@@ -1,10 +1,19 @@
 use std::ffi::CString;
+use std::option::Option;
 
 use nix::unistd::fork;
 use nix::unistd::ForkResult;
 
 use crate::shell::Shell;
 use crate::utility::split_no_empty;
+
+fn is_exec(path: &str) -> Result<(), String> {
+    if let Err(err) = nix::unistd::access(path, nix::unistd::AccessFlags::X_OK) {
+        return Err(String::from(err.as_errno().unwrap().desc()));
+    }
+
+    return Ok(());
+}
 
 impl Shell {
     fn wait_for_child(self: &mut Shell, child_pid: nix::unistd::Pid) {
@@ -15,6 +24,27 @@ impl Shell {
             Ok(_) => println!("other"),
             Err(err) => println!("{:?}", err),
         }
+    }
+
+    fn get_command_path(self: &Shell, command: &String) -> Result<CString, String> {
+        if command.contains('/') {
+            return match is_exec(command) {
+                Ok(_) => Ok(CString::new(command.clone()).unwrap()),
+                Err(err) => Err(err),
+            };
+        }
+
+        let paths = self.env["PATH"].split(':');
+
+        for p in paths {
+            let full_command = format!("{}/{}", p, command);
+
+            if let Ok(_) = is_exec(&full_command) {
+                return Ok(CString::new(full_command.as_str()).unwrap());
+            }
+        }
+
+        return Err(format!("{}: Command not found.", command).to_string());
     }
 
     fn child_exec_command(self: &mut Shell, command: &Vec<String>) {
@@ -31,21 +61,21 @@ impl Shell {
             .collect();
         let cenv = &cenv[..];
 
-        let path = CString::new(command[0].clone()).unwrap();
-
-        match nix::unistd::execve(&path, &args, &cenv) {
-            Ok(_) => unimplemented!(),
-            Err(err) => println!("{:?}", err),
+        match self.get_command_path(&command[0]) {
+            Ok(path) => {
+                if let Err(err) = nix::unistd::execve(&path, &args, &cenv) {
+                    println!("{}", err.as_errno().unwrap().desc());
+                }
+            }
+            Err(err) => println!("{}", err),
         };
+        std::process::exit(1);
     }
 
     fn exec_command(self: &mut Shell, command: &Vec<String>) {
         match fork() {
             Ok(ForkResult::Parent { child }) => self.wait_for_child(child),
-            Ok(ForkResult::Child) => {
-                self.child_exec_command(&command);
-                std::process::exit(1);
-            }
+            Ok(ForkResult::Child) => self.child_exec_command(&command),
             Err(err) => println!("{:?}", err),
         }
     }
